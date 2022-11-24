80e3bf12c33a9e69972c555e60645b140b3f6b00
stepchik003 <stepan.alex.003@gmail.com>
2022-11-24 20:45:23 +0300

    seventh lesson corrected

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -300,7 +300,7 @@ fun markdownToHtmlSimple(inputName: String, outputName: String) {
     var generalFlag = 0
     var k = 0
     val lines = File(inputName).readLines()
-    if (lines.last().isEmpty()) lines.toMutableList().remove(lines.last())
+    if (lines.last().isEmpty() && lines.size > 1) lines.toMutableList().remove(lines.last())
     for (line in lines) {
         line.trim()
         if (line.isEmpty() || line.matches(Regex("\\s+"))) {
