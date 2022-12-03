a0fc59378af442a9f8e5e48cac610dbda134c4e1
stepchik003 <stepan.alex.003@gmail.com>
2022-12-03 19:15:53 +0300

    seventh lesson done

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -576,9 +576,9 @@ fun printDivisionProcess(lhv: Int, rhv: Int, outputName: String) {
     writer.write("$lhv | $rhv\n")
     for (i in 1..digitNumber(partly)) {
         d = rhv * partList[i - 1]
-        if (digitNumber(lastNum) == digitNumber(d) && lastNum != 0 && d != 0 && ind >= 1 && lastNum - d != 0) {
-            ind--
-        }
+        if (digitNumber(lastNum) == digitNumber(d) && lastNum != 0 && d != 0 && ind >= 1
+            && lastNum - d != 0 && diff != 0
+        ) ind--
         if (i == digitNumber(partly) && lastNum - d == 0 && diff != 0 && ind >= 1) ind--
         writer.write(" ".repeat(ind) + "-$d")
         if (i == 1) writer.write(" ".repeat(digitNumber(lhv) - digitNumber(d) - fl + 3) + partly)
